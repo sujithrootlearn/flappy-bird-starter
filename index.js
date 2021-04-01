994a99fb8f4e89bbdc83117f417f4e356da75bde
Manish Poduval <manishpoduval@hotmail.com>
2021-04-01 12:11:09 +0530

    starter

---
+++ index.js
@@ -0,0 +1,36 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+canvas.style.border = '2px solid black';
+
+// load all images
+let bg = new Image();
+bg.src = './images/bg.png';
+
+let fg = new Image();
+fg.src = './images/fg.png';
+
+let bird = new Image();
+bird.src = './images/bird.png'
+
+let pipeNorth = new Image();
+pipeNorth.src = './images/pipeNorth.png'
+
+let pipeSouth = new Image();
+pipeSouth.src = './images/pipeSouth.png'
+
+let intervalId = 0;
+let isGameOver = false;
+
+
+// basic animation template
+function draw(){
+    
+    if (isGameOver) {
+        cancelAnimationFrame(intervalId)
+        return;
+    }
+    intervalId = requestAnimationFrame(draw)
+}
+
+
+draw()
